662deae4dca19d2b5d86dfe9885fe424c993454d
makenma <975245937@qq.com>
2026-04-06 23:11:30 +0800

    mem-cache: update sink script
    Change-Id: I2089955af15a31468527ad63251160b26a35e871

--- src/mem/cache/CHI/ChiFlitSink.py
+++ src/mem/cache/CHI/ChiFlitSink.py
@@ -5,6 +5,6 @@ from m5.objects.ClockedObject import ClockedObject
 class ChiFlitSink(ClockedObject):
     type = "ChiFlitSink"
     cxx_header = "mem/cache/CHI/ChiFlitSink.hh"
-    cxx_class = "gem5::ChiFlitSink"
+    cxx_class = "gem5::Chi::ChiFlitSink"
 
     chi_side = ResponsePort("Sink CHI side port")
