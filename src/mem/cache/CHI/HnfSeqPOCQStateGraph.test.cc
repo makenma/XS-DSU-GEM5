ea1d65e93adef8bd21e4551916238da54d0972e6
makenma <975245937@qq.com>
2026-07-12 10:59:00 +0800

    mem-cache: test HNF SEQ POCQ hazards
    Change-Id: If2b336f35ae7270c3d9eb31deb56f4b2ca42117a

---
+++ src/mem/cache/CHI/HnfSeqPOCQStateGraph.test.cc
@@ -0,0 +1,51 @@
+#include <gtest/gtest.h>
+
+#include "mem/cache/CHI/HnfSeqPOCQStateGraph.hh"
+
+namespace gem5::Chi
+{
+
+TEST(HnfSeqPocqStateGraphTest, NoHazardSnoopsThenCompletes)
+{
+    SEQ_POCQ_StateGraph graph;
+    SeqPocqState state = SeqPocqState::Idle;
+
+    auto step = graph.tryStep(state, {SeqPocqEventKind::Admit});
+    ASSERT_TRUE(step.stepped);
+    EXPECT_EQ(state, SeqPocqState::HazardCheck);
+    ASSERT_EQ(step.actions.size(), 1);
+    EXPECT_EQ(step.actions[0], SeqPocqActionKind::CheckHazard);
+
+    step = graph.tryStep(state, {SeqPocqEventKind::HazardClear});
+    ASSERT_TRUE(step.stepped);
+    EXPECT_EQ(state, SeqPocqState::WaitSnoop);
+    ASSERT_EQ(step.actions.size(), 1);
+    EXPECT_EQ(step.actions[0], SeqPocqActionKind::QueueCleanInvalid);
+
+    step = graph.tryStep(state, {SeqPocqEventKind::SnoopDone});
+    ASSERT_TRUE(step.stepped);
+    EXPECT_EQ(state, SeqPocqState::Idle);
+    ASSERT_EQ(step.actions.size(), 2);
+    EXPECT_EQ(step.actions[0], SeqPocqActionKind::CompleteSfEvict);
+    EXPECT_EQ(step.actions[1], SeqPocqActionKind::Retire);
+}
+
+TEST(HnfSeqPocqStateGraphTest, AddressHazardSleepsUntilClear)
+{
+    SEQ_POCQ_StateGraph graph;
+    SeqPocqState state = SeqPocqState::Idle;
+
+    ASSERT_TRUE(graph.tryStep(state, {SeqPocqEventKind::Admit}).stepped);
+    auto step = graph.tryStep(state, {SeqPocqEventKind::HazardBlocked});
+    ASSERT_TRUE(step.stepped);
+    EXPECT_EQ(state, SeqPocqState::Sleep);
+    EXPECT_TRUE(step.actions.empty());
+
+    step = graph.tryStep(state, {SeqPocqEventKind::HazardClear});
+    ASSERT_TRUE(step.stepped);
+    EXPECT_EQ(state, SeqPocqState::WaitSnoop);
+    ASSERT_EQ(step.actions.size(), 1);
+    EXPECT_EQ(step.actions[0], SeqPocqActionKind::QueueCleanInvalid);
+}
+
+} // namespace gem5::Chi
